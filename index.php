685c9c025576a86b41aa77520d149f63766f497c
Dan Phiffer <dan@phiffer.org>
2016-07-12 11:37:52 -0400

    :wrench: Error handling; smarter caching

--- index.php
+++ index.php
@@ -28,8 +28,16 @@ if (preg_match('/^\/(\d+)/', $_SERVER['REQUEST_URI'], $matches)) {
 		curl_setopt($ch, CURLOPT_HEADER, 0);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		$json = curl_exec($ch);
+		$status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+		if ($status > 299) {
+			include 'error.html';
+			exit;
+		}
+
 		curl_close($ch);
-		if (is_writable(__DIR__ . "/cache")) {
+		if (is_writable(__DIR__ . "/cache") &&
+		    $status == 200) {
 			mkdir(dirname(__DIR__ . "/cache/$path"), 0755, true);
 			file_put_contents(__DIR__ . "/cache/$path", $json);
 		}
@@ -37,9 +45,9 @@ if (preg_match('/^\/(\d+)/', $_SERVER['REQUEST_URI'], $matches)) {
 	$wof = json_decode($json, 'as hash');
 	$props = $wof['properties'];
 	if (! empty($props['wof:name'])) {
-		$title = $props['wof:name'];
+		$title = "{$props['wof:name']} | All of the Places";
 	} else {
-		$title = "Place $id";
+		$title = "Place $id | All of the Places";
 	}
 	if (! empty($props['geom:bbox'])) {
 		$bbox = $props['geom:bbox'];
@@ -94,11 +102,13 @@ if (preg_match('/^\/(\d+)/', $_SERVER['REQUEST_URI'], $matches)) {
 			}
 
 		?>></div>
+		<?php if (!empty($json)) { ?>
 		<script>
 
 		var wof = <?php echo $json; ?>;
 
 		</script>
+		<?php } ?>
 		<script src="https://mapzen.com/common/styleguide/scripts/mapzen-styleguide.min.js"></script>
 		<script src="/jquery-3.0.0.min.js"></script>
 		<script src="/lib/leaflet-routing-machine/dist/leaflet-routing-machine.min.js"></script>
